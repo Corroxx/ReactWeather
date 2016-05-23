c71ae058af121cffaacf0be1f54f66e1c1c89ecb
Andreas Buehler <andreasbuehler@me.com>
2016-05-24 00:51:00 +0200

    About Paragraph added

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use (function (req, res, next){
   if(req.headers['x-forwarded-proto'] === 'http'){
     next();
   } else {
-    res.redirect ('http://' + req.hostneme + req.url);
+    res.redirect ('http://' + req.hostname + req.url);
   }
 });
 
 app.use(express.static('public'));
 app.listen(PORT, function(){
-    console.log('Express is up and running on port'+ PORT);
+    console.log('Express is up and running on port '+ PORT);
 });
