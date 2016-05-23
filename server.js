7b55d7dfe28a225452d5b815c40e85d4f6a50d55
Andreas Buehler <andreasbuehler@me.com>
2016-05-23 23:47:12 +0200

    prep Repo for Heroku

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ var express = require('express');
 
 //Server
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use (function (req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect ('http://' + req.hostneme + req.url);
+  }
+});
 
 app.use(express.static('public'));
-app.listen(3000, function(){
-    console.log('Express is up and running');
+app.listen(PORT, function(){
+    console.log('Express is up and running on port'+ PORT);
 });
