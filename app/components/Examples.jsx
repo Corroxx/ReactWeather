4fb9554ddb18394c8e04558ee8362c0151eaad51
Andreas Buehler <andreasbuehler@me.com>
2016-05-24 17:10:28 +0200

    Foundation Grid style

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,19 +1,19 @@
 var React = require('react');
-
-// var Examples = React.createClass({
-//   render: function(){
-//     return (
-//       <h3>Examples</h3>
-//     );
-//   }
-// });
+var {Link} = require('react-router');
 
 var Examples =(props) => {
   return (
     <div>
-      <h3>Examples Component !</h3>
-      <p>This is the Examples Page!</p>
-
+      <h1 className="text-center">Examples Component !</h1>
+      <p>Here are a few example Locations to try out!</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
   )
 };
