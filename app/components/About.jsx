f8bdbe2e690822bc541ca71f64b625c2b6a94d26
Andreas Buehler <andreasbuehler@me.com>
2016-05-24 19:09:46 +0200

    About Page redesigned

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,19 +1,23 @@
 var React = require('react');
-
-/*var About = React.createClass({
-  render: function (){
-    return(
-      <h3>About Component</h3>
-    );
-  }
-});*/
+var {Link} = require('react-router');
 
 var About = (props) => {
   return(
       <div>
-        <h3>About</h3>
+        <h1 className="text-center">About</h1>
         <p>Wecome to the About Page!</p>
-      </div>
+        <p>We offer different services round the globe.</p>
+        <ol>
+          <li>Weather Services</li>
+          <li>Astronautic Information</li>
+          <li>Weather forecasts</li>
+          <li>Exact humidity values</li>
+        </ol>
+        <p>Try it out we have provided some Examples for you!</p>
+        <li>
+          <Link to='/examples'>Examples</Link>
+        </li>
+    </div>
   );
 };
 module.exports = About;
