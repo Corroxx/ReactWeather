8770fae95105760ba706f523c9ac57e72f6b2c74
Andreas Buehler <andreasbuehler@me.com>
2016-05-29 18:48:19 +0200

    Grid angepasst für Menü rechts uns links

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,9 +5,15 @@ var Main = (props) =>{
   return(
       <div>
           <Nav/>
-          <div className="row">
-            <div className="columns medium-6 large-4 small-centered">
+          <div className="expanded row">
+            <div className="columns medium 3 large-4">
+              <h3>Dies ist eine Textnachricht im Grid links!</h3>
+            </div>
+            <div className="columns medium-6 large-4">
               {props.children}
+            </div>
+            <div className="columns medium 3 large-4">
+              <h3>Dies ist eine Textnachricht im Grid rechts</h3>
             </div>
           </div>
       </div>
